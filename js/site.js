517d5259c9d85619714878b272b03cd255f7ba6a
Joel Glovier <jglovier@gmail.com>
2020-12-26 15:35:09 -0800

    initial commit (dump from source repo)
    This repo was created from a previous commit on my main website repo, commit 2edc983e5fec59f2505e37e8298b685ac80bb9be. You can see it at https://github.com/jglovier/jglovier.github.io/commit/2edc983e5fec59f2505e37e8298b685ac80bb9be.

---
+++ js/site.js
@@ -0,0 +1,52 @@
+$(function(){
+
+  // Homepage | section.writing | article nav behavior
+  // NOTE: this code is mad ghetto and in no wise to be held up as a standard of JavaScript brilliance; so sorry bout that; but It Works™
+  $("#articlesAllTrigger").on("click", function(event){
+    event.preventDefault();
+    $(".article-nav .year").removeClass("showing");
+    $(this).addClass("showing");
+    $("div.articles").addClass("show");
+  });
+
+  $("#articles2015Trigger").on("click", function(event){
+    event.preventDefault();
+    $(".article-nav .year").removeClass("showing");
+    $(this).addClass("showing");
+    $("div.articles").removeClass("show");
+    $("#articles2015").addClass("show");
+  });
+
+  $("#articles2014Trigger").on("click", function(event){
+    event.preventDefault();
+    $(".article-nav .year").removeClass("showing");
+    $(this).addClass("showing");
+    $("div.articles").removeClass("show");
+    $("#articles2014").addClass("show");
+  });
+
+  $("#articles2013Trigger").on("click", function(event){
+    event.preventDefault();
+    $(".article-nav .year").removeClass("showing");
+    $(this).addClass("showing");
+    $("div.articles").removeClass("show");
+    $("#articles2013").addClass("show");
+  });
+
+  $("#articles2012Trigger").on("click", function(event){
+    event.preventDefault();
+    $(".article-nav .year").removeClass("showing");
+    $(this).addClass("showing");
+    $("div.articles").removeClass("show");
+    $("#articles2012").addClass("show");
+  });
+
+  $("#articles2011Trigger").on("click", function(event){
+    event.preventDefault();
+    $(".article-nav .year").removeClass("showing");
+    $(this).addClass("showing");
+    $("div.articles").removeClass("show");
+    $("#articles2011").addClass("show");
+  });
+
+});
